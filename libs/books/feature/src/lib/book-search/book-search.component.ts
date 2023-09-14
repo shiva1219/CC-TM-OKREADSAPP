2dfd6e26e80fc2a45a803fa3b7b0fa6570c57b72
Shiva <shiva1219@gmail.com>
2023-09-14 14:09:06 -0500

    Task 1: Code fixes and review

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -4,7 +4,6 @@ import {
   addToReadingList,
   clearSearch,
   getAllBooks,
-  ReadingListBook,
   searchBooks
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
@@ -16,8 +15,8 @@ import { Book } from '@tmo/shared/models';
   styleUrls: ['./book-search.component.scss']
 })
 export class BookSearchComponent implements OnInit {
-  books: ReadingListBook[];
 
+  books$ = this.store.select(getAllBooks); 
   searchForm = this.fb.group({
     term: ''
   });
@@ -32,9 +31,6 @@ export class BookSearchComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.store.select(getAllBooks).subscribe(books => {
-      this.books = books;
-    });
   }
 
   formatDate(date: void | string) {
